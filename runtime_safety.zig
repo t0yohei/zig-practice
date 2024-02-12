3514b853bb9496aecc78a28366a8be9adbc7d01a
t0yohei <inuneko690121@gmail.co>
2024-02-12 16:11:37 +0900

    add runtime_safety and pointers

---
+++ runtime_safety.zig
@@ -0,0 +1,28 @@
+const expect = @import("std").testing.expect;
+
+test "out of bounds, no safety" {
+    @setRuntimeSafety(false);
+    const a = [3]u8{ 1, 2, 3 };
+    var index: u8 = 5;
+    const b = a[index];
+    _ = b;
+}
+
+// test "unreachable" {
+//     const x: i32 = 1;
+//     const y: u32 = if (x == 2) 5 else unreachable;
+//     _ = y;
+// }
+
+fn asciiToUpper(x: u8) u8 {
+    return switch (x) {
+        'a'...'z' => x + 'A' - 'a',
+        'A'...'Z' => x,
+        else => unreachable,
+    };
+}
+
+test "unreachable switch" {
+    try expect(asciiToUpper('a') == 'A');
+    try expect(asciiToUpper('A') == 'A');
+}
